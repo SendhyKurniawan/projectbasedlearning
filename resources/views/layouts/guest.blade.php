48705b95c52676e046e537e63aff4ff4416854aa
sendhykurniawan <sendhy27@gmail.com>
2026-05-28 17:59:16 +0700

    feat(branding): add school-icon favicon matching sidebar logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,9 @@
  <meta charset="utf-8">
  <meta name="viewport" content="width=device-width, initial-scale=1">
  <meta name="csrf-token" content="{{ csrf_token() }}">
+ <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+ <link rel="alternate icon" href="{{ asset('favicon.ico') }}">
+ <link rel="apple-touch-icon" href="{{ asset('favicon.svg') }}">
  <script>
   (function(){
    document.documentElement.classList.remove('dark');
